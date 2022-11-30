f5d83cc61765f6994f4da86ce9d79605d6871d10
Denis Khmelnov <denishmelnov@gmail.com>
2022-11-30 23:02:00 +0500

    feat: render config

--- app.py
+++ app.py
@@ -3,15 +3,14 @@ from flask import Flask
 from db import db
 from views import main_bp
 
-DB_USER = 'db_user'
-DB_PASSWORD = 'db_password'
-DB_NAME = 'db_name'
-DB_PORT = 5434
-DB_HOST = '127.0.0.1'
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_NAME = os.getenv('DB_NAME')
+DB_HOST = 'db'
 
 def create_app() -> Flask:
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     app.register_blueprint(main_bp)
